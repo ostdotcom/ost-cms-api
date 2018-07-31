92f371134a5e435ab03f784da28129e06ad3e560
Akshay <akshay.raje@gmail.com>
2018-07-31 15:48:46 +0530

    Google auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    puts '--------------------'
-    puts session[:user_id]
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
@@ -9,4 +10,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path
   end
+
 end
