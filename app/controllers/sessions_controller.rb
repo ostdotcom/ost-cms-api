32159854acfcd5b3e309aeda5d7256d68a134d8a
mayurpatil888 <mayur@ost.com>
2018-08-01 13:14:02 +0530

    auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
     session[:user_id] = @user.id
-    redirect_to :me
+    redirect_to :dashboard
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'login', to: redirect('/auth/google_oauth2'), as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'api/login', to: redirect('/auth/google_oauth2'), as: 'login'
+  get 'api/logout', to: 'sessions#destroy', as: 'logout'
   get 'auth/:provider/callback', to: 'sessions#create'
-  post 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'home', to: 'home#show'
-  get 'me', to: 'me#show', as: 'me'
-
+  get 'dashboard', to: redirect('/dashboard')
   root to: "home#show"
 
 end
