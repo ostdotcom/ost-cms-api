a65672d10f09b4485b5b8490e963fd6cb38499aa
Preshita Shirke <preshita@ost.com>
2018-08-20 13:12:45 +0530

    uncommit

--- app/services/db_service/base.rb
+++ app/services/db_service/base.rb
@@ -100,5 +100,18 @@ module DbService
       end
     end
 
+
+    def min_bytes(validation, input)
+
+    end
+
+    def max_bytes(validation, input)
+
+    end
+
+    def accept(validation, input)
+
+    end
+
   end
 end
