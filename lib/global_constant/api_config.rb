4f99cd69ca484592e9f0708c3778616754e33301
mayurpatil888 <mayur@ost.com>
2018-09-20 11:24:49 +0530

    review changes

--- lib/global_constant/api_config.rb
+++ lib/global_constant/api_config.rb
@@ -5,11 +5,19 @@ module GlobalConstant
 
     class << self
 
-      def fetch_config
+      def fetch_config()
         @fetch_config ||= begin
           template = ERB.new File.new("#{Rails.root}/config/api_config.yml").read
           YAML.load(template.result(binding)) || {}
         end
+      end
+
+      def fetch_entity_config(entity)
+          template = ERB.new File.new("#{Rails.root}/config/api_config.yml").read
+          config = YAML.load(template.result(binding))
+          entity_config = config["meta"][entity.to_sym]
+          entity_info = Entity.find_by_name(entity)
+          {"fields" => entity_config,  "entity_type" => entity_info.configuration, "entity_name" => entity_info.name  }
       end
 
     end
