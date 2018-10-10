710e5378c2e0397a7972726d4a7fdc5bc77b5a9b
mayurpatil888 <mayur@ost.com>
2018-10-10 18:00:06 +0530

    cms env var

--- lib/order_weights.rb
+++ lib/order_weights.rb
@@ -3,7 +3,7 @@ class OrderWeights
   def get_new_record_weight(entity_id)
 
     if EntityDataVersion.where(:entity_id => entity_id).where(:status => [GlobalConstant::Models::EntityDataVersion.draft, GlobalConstant::Models::EntityDataVersion.active]).count == 0
-      weight = 100000000000000000000000.0
+      weight = GlobalConstant::AppConfig.new_entity_order_weight
     else
       weight = get_min_weight(entity_id) * (9.0 /10.0)
     end
